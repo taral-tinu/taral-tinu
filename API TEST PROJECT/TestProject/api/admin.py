15d15bc56e8543556e5d36853d5b910d7d318f61
Taraltinu <tinu1316@gmail.com>
2022-01-25 22:33:08 +0530

    changes

--- API TEST PROJECT/TestProject/api/admin.py
+++ API TEST PROJECT/TestProject/api/admin.py
@@ -4,11 +4,8 @@ from django.contrib import admin
 from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 
-from .models import (CollectionAction, ContentPermission, Country, Customer,
-                     GroupPermission, Invoice, MainMenu, PagePermission,
-                     Scheduler, SchedulerItem, UserGroup, UserProfile)
-
-
+from .models import ( ContentPermission, GroupPermission,  MainMenu, PagePermission,
+                       UserGroup)
 class MainMenuAdmin(admin.ModelAdmin):
     list_display = ("name", "url", "icon", "parent_id", "sequence", "menu_code")
 
@@ -29,21 +26,21 @@ class CollectionActionAdmin(admin.ModelAdmin):
 class UserProfileAdmin(admin.ModelAdmin):
     list_display = ("is_active")
 
-@admin.register(SchedulerItem)
-class SchedulerItemAdmin(admin.ModelAdmin):
-    list_display = ["scheduler","customer"]
+# @admin.register(SchedulerItem)
+# class SchedulerItemAdmin(admin.ModelAdmin):
+#     list_display = ["scheduler","customer"]
 
-@admin.register(Scheduler)
-class SchedulerItemAdmin(admin.ModelAdmin):
-    list_display = ["scheduler_name","created_on","status"]
+# @admin.register(Scheduler)
+# class SchedulerItemAdmin(admin.ModelAdmin):
+#     list_display = ["scheduler_name","created_on","status"]
 
 
-admin.site.register(Invoice,InvoiceAdmin)
-admin.site.register(Customer)
-admin.site.register(CollectionAction,CollectionActionAdmin)
+# admin.site.register(Invoice,InvoiceAdmin)
+# admin.site.register(Customer)
+# admin.site.register(CollectionAction,CollectionActionAdmin)
 # admin.site.register(Scheduler)
 # admin.site.register(SchedulerItem)
-admin.site.register(Country)
+# admin.site.register(Country)
 # admin.site.register(UserProfile)
 admin.site.register(GroupPermission)
 admin.site.register(UserGroup)
